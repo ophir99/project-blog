cef6c0502c8b8264fdadbba88e76a37bbe6dc29b
Prasad Kadiyala <external.pvkadiyala@atheerair.com>
2019-03-11 12:17:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from "@angular/material";
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-one';
+  columns_ = ['id', 'name']
+  data = [
+    {
+      id: 1,
+      name: "Angular"
+    },
+    {
+      id: 2,
+      name: "Vue"
+    }
+  ];
+  posts;
+
+  @ViewChild(MatPaginator)
+  paginator;
+  @ViewChild(MatSort)
+  matSort;
+  dataSource = new MatTableDataSource();
+  columns1 = ["id", "title", "body"];
+  dataSource1 = new MatTableDataSource();
+  constructor(
+    private http: HttpClient
+  ){
+    this.http.get(`https://jsonplaceholder.typicode.com/posts`)
+      .subscribe(
+        (res: any) => {
+          this.posts = res;
+          this.dataSource1.data = res;
+          this.dataSource1.paginator = this.paginator;
+          this.dataSource1.sort = this.matSort;
+          this.dataSource1.filter = "l"
+        }
+      )
+    this.dataSource.data = this.data;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule, MatTableModule, MatPaginator, MatPaginatorModule, MatSortModule } from "@angular/material";
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+  HttpClientModule,
+  MatPaginatorModule,
+  MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
